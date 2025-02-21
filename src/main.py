0058891d8499d740663ef652eae2a5fb5f8053a0
Leonov Egor <crozetle@gmail.com>
2025-02-21 20:09:10 +0300

    Вроде как практически все соединил

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-
+from fastapi.staticfiles import StaticFiles
 from src.router import router as router_base
+import os
+import config
 
 app = FastAPI()
 
 app.include_router(router_base)
+app.mount("/static", StaticFiles(directory= config.PATH / "src/frontend/webpage"), name="static")
