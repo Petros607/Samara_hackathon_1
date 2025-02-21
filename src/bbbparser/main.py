06d46b34291db5b1913ad4afbcb067a149a64d27
Leonov Egor <crozetle@gmail.com>
2025-02-21 13:12:24 +0300

    Доработал у всех работу с PATH

--- src/bbbparser/main.py
+++ src/bbbparser/main.py
@@ -4,6 +4,8 @@ import urllib
 import os
 from bs4 import BeautifulSoup
 import subprocess
+import pathlib
+from config import PATH
 
 TEST_URL = "https://bbb.ssau.ru:8443/playback/presentation/2.3/aa788c4e3599195fe1a7a12ad585903f44c6f9ed-1739185560849"
 
@@ -13,6 +15,9 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 
+PATH_files = pathlib.Path(PATH) / "data/temporaryFiles"
+
+
 class Parser:
 
     def get_data(self, url: str):
@@ -21,16 +26,21 @@ class Parser:
         string = ""
         for key in content.keys():
             string = string + key + " "
-        return "https://bbb.ssau.ru:8443/presentation/" + url.split("/")[6] + "/presentation/" + string.split(" ")[1] + "/svgs/slide" + "&" + string.split(" ")[1]
+        return "https://bbb.ssau.ru:8443/presentation/" + url.split("/")[6] + "/presentation/" + string.split(" ")[
+            1] + "/svgs/slide" + "&" + string.split(" ")[1]
 
     def download_audio(self, url: str):
         id = url.split("/")[4]
         audio_id = url.split("&")[0].split("/")[4]
         audio_url = "https://bbb.ssau.ru:8443/presentation/" + audio_id + "/video/webcams.webm"
-        if not os.path.exists(id):
-            os.makedirs(id)
-        if not os.path.exists(id + "/audio"):
-            os.makedirs(id + "/audio")
+
+        path_id = pathlib.Path(PATH_files) / id
+
+        if not os.path.exists(path_id):
+            os.makedirs(path_id)
+        if not os.path.exists(path_id / "audio"):
+            os.makedirs(path_id / "audio")
+
         urllib.request.urlretrieve(audio_url, id + "/audio/" + 'lecture.webm')
         return 0
 
@@ -38,16 +48,20 @@ class Parser:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
         lection_title = soup.find('h1', class_="display-3 text-left mb-3 font-weight-400").get_text(strip=True)
-        teacher_name = soup.find('h5', id="room-owner-name",class_="font-weight-normal ml-4 mt-3 d-inline-block").get_text(strip=True).replace(" (Владелец)", "")
+        teacher_name = soup.find('h5', id="room-owner-name",
+                                 class_="font-weight-normal ml-4 mt-3 d-inline-block").get_text(strip=True).replace(
+            " (Владелец)", "")
         lections_table = soup.find_all("table", id="recordings-table")[0]
         lections = lections_table.find_all("tr")[2:]
         i = 0
         data = {}
         for lection in lections:
             time = lection.find("time").get_text(strip=True)
-            lection_url = lection.find("a", class_="btn btn-sm btn-primary", target="_blank").get_attribute_list("href")[0]
-            dict = { f'lection_{i}':{
-                "id": None, "name_file": None, "name_teacher": teacher_name,"url": lection_url, "name_subject": lection_title,
+            lection_url = \
+                lection.find("a", class_="btn btn-sm btn-primary", target="_blank").get_attribute_list("href")[0]
+            dict = {f'lection_{i}': {
+                "id": None, "name_file": None, "name_teacher": teacher_name, "url": lection_url,
+                "name_subject": lection_title,
                 "datetime": time, "lenght": None, "path": None, "size": None
             }}
             data.update(dict)
@@ -72,19 +86,22 @@ class Parser:
     def download_image(self, url: str):
         i = 1
         url = url.split("&")[0]
-        dir_name = url.split("/")[4]
-        if not os.path.exists(dir_name):
-            os.makedirs(dir_name)
-        if not os.path.exists(dir_name + "/slides"):
-            os.makedirs(dir_name + "/slides")
+        id = url.split("/")[4]
+
+        path_id = pathlib.Path(PATH_files) / id
+
+        if not os.path.exists(path_id):
+            os.makedirs(path_id)
+        if not os.path.exists(path_id / 'slides'):
+            os.makedirs(path_id / "slides")
         while True:
             try:
-                save_path = os.path.join(dir_name + "/slides", "slide" + str(i) + ".svg")
+                save_path = path_id / f"/slides/slide{str(i)}.svg"
                 response = requests.get(url + str(i) + ".svg")
                 if response.status_code == 200:
                     with open(save_path, "wb") as file:
                         file.write(response.content)
-                    i+=1
+                    i += 1
                 else:
                     break
             except:
